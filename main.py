fc3fa42de07e19685e534d9da13639332b01d813
Robert Pliszka <robplisz@gmail.com>
2026-09-09 16:26:46 -0400

    Wrote code to retrive drink from manu and create object depending on drink choice

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from money_machine import MoneyMachine
 machine_on = True
 coffee_maker = CoffeeMaker()
 machine_money = MoneyMachine()
-coffee_options = Menu()
+menu = Menu()
+is_on = True
 
-coffee_maker.report()
-machine_money.report()
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What do you want to do? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        machine_money.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(drink)
