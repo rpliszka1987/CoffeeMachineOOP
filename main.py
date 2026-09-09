93dbca878bd58685c2c04fda6e2638f9eb270a7a
Robert Pliszka <robplisz@gmail.com>
2026-09-09 16:20:22 -0400

    Coffee maker generates report from using object to show resources in machine

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+machine_on = True
+coffee_maker = CoffeeMaker()
+machine_money = MoneyMachine()
+coffee_options = Menu()
+
+coffee_maker.report()
+machine_money.report()
